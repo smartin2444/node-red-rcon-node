b93008e30dced18e4ce777b2847a27201106b5e1
smartin2444 <145486140+smartin2444@users.noreply.github.com>
2024-05-13 23:30:30 +0200

    ultim update

--- rcon-command.js
+++ rcon-command.js
@@ -1,24 +1,10 @@
-
 const Rcon = require("rcon");
 
-/*let client = new Rcon("localhost", 25575, "Test1234");
-
-client.on("auth", function(){
-        console.log("auth")
-        client.send("tps")
-}).on("response", function(str){
-        console.log("RESP",str);
-        client.disconnect()
-})
-client.connect();*/
-
-
 module.exports = function(RED) {
-    function LowerCaseNode(config) {
+    function RCONNode(config) {
         RED.nodes.createNode(this,config);
         var node = this;
-
-        let client = null; //new Rcon("172.20.17.247", 25575, "Test1234");
+        let client = null;
 	let clientAuthenticated = false;
 	let lastmsg = {}
 	let connIp = "0.0.0.0"
@@ -26,28 +12,20 @@ module.exports = function(RED) {
 	let connPassword = ""
 
 	function authenticateRcon(ip, port, password){
-		//console.log("trying")
 		clientAuthenticated = false;
 		if(!(ip && port && password)){
 			return null
 		}
-
 		let client = new Rcon(ip, port, password);
 		client.on("auth", function(){
 			clientAuthenticated = true;
-			//lastmsg = msg
-			//client.send(msg.payload)
-			//msg.payload = msg.payload.toLowerCase();
-			//node.send(msg);
 		});
 		client.on("response", function(str){
-			//console.log("RESP",str);
 			if(lastmsg && str){
 				lastmsg.payload = str;
 				lastmsg.error = false
 				node.send.call(node,lastmsg);
 			}
-			//client.disconnect()
 		})
 		client.on("error", function(str){
 			console.log("Error",str)
@@ -56,29 +34,27 @@ module.exports = function(RED) {
 			node.send.call(node,lastmsg)
 		})
 		client.connect();
-		//console.log("opened")
 		return client;
 	}
-
-	
 	node.on("input", function(msg){
 		if(!msg) return;
-
 		if(msg.options){
 			if(msg.options.ip){
 				connIp = msg.options.ip;
+				if(client) client.disconnect();
 				client = null;
 			}
 			if(msg.options.port){
 				connPort = msg.options.port;
+				if(client) client.disconnect();
 				client = null;
 			}
 			if(msg.options.password){
 				connPassword = msg.options.password;
+				if(client) client.disconnect();
 				client = null;
 			}
 		}
-	
 		if(client == null){
 			client = authenticateRcon(connIp, connPort, connPassword)
 		}
@@ -86,12 +62,8 @@ module.exports = function(RED) {
 			lastmsg = msg;
 			client.send(msg.payload);
 		}
-
-
-
-
 	})
 
     }
-    RED.nodes.registerType("rcon-command",LowerCaseNode);
+    RED.nodes.registerType("rcon-command",RCONNode);
 }
