9ebcd5c7d36885073858b9d23619572be8a9d06c
smartin2444 <145486140+smartin2444@users.noreply.github.com>
2024-05-13 23:30:30 +0200

    aab

--- rcon-command.js
+++ rcon-command.js
@@ -17,28 +17,81 @@ module.exports = function(RED) {
     function LowerCaseNode(config) {
         RED.nodes.createNode(this,config);
         var node = this;
-        let client = new Rcon("172.20.17.247", 25575, "Test1234");
+
+        let client = null; //new Rcon("172.20.17.247", 25575, "Test1234");
+	let clientAuthenticated = false;
 	let lastmsg = {}
+	let connIp = "0.0.0.0"
+	let connPort = 25575
+	let connPassword = ""
+
+	function authenticateRcon(ip, port, password){
+		//console.log("trying")
+		clientAuthenticated = false;
+		if(!(ip && port && password)){
+			return null
+		}
+
+		let client = new Rcon(ip, port, password);
+		client.on("auth", function(){
+			clientAuthenticated = true;
+			//lastmsg = msg
+			//client.send(msg.payload)
+			//msg.payload = msg.payload.toLowerCase();
+			//node.send(msg);
+		});
+		client.on("response", function(str){
+			//console.log("RESP",str);
+			if(lastmsg && str){
+				lastmsg.payload = str;
+				lastmsg.error = false
+				node.send.call(node,lastmsg);
+			}
+			//client.disconnect()
+		})
+		client.on("error", function(str){
+			console.log("Error",str)
+			let newmsg = lastmsg
+			newmsg.error = true
+			node.send.call(node,lastmsg)
+		})
+		client.connect();
+		//console.log("opened")
+		return client;
+	}
+
+	
+	node.on("input", function(msg){
+		if(!msg) return;
+
+		if(msg.options){
+			if(msg.options.ip){
+				connIp = msg.options.ip;
+				client = null;
+			}
+			if(msg.options.port){
+				connPort = msg.options.port;
+				client = null;
+			}
+			if(msg.options.password){
+				connPassword = msg.options.password;
+				client = null;
+			}
+		}
+	
+		if(client == null){
+			client = authenticateRcon(connIp, connPort, connPassword)
+		}
+		if(clientAuthenticated){
+			lastmsg = msg;
+			client.send(msg.payload);
+		}
 
 
 
-        client.on("auth", function(){
-            node.on('input', function(msg) {
-		lastmsg = msg
-                client.send(msg.payload)                
-                //msg.payload = msg.payload.toLowerCase();
-                //node.send(msg);
-            });
-        })
-        client.on("response", function(str){
-            console.log("RESP",str);
-            lastmsg.payload = str;
-            node.send(lastmsg);
-            //client.disconnect()
-        })
-        client.connect();
 
-        
+	})
+
     }
     RED.nodes.registerType("rcon-command",LowerCaseNode);
 }
